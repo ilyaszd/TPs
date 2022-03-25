a77c345d5f3bde9fdb0989406b1a366e3b8cdad7
ilyas <ilyaszaidoune@gmail.com>
2022-03-25 16:37:38 +0100

    tp5

---
+++ src/main/java/ma/education/tp5/collections/Client.java
@@ -0,0 +1,29 @@
+package ma.education.tp5.collections;
+
+public class Client {
+    Integer code;
+    String name;
+
+    public Client(Integer code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "code=" + code +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+
+    /*
+    @Override
+    public int compareTo(Object o) {
+        Client client = (Client) o;
+        return -client.code+this.code;
+    }
+
+     */
+}
